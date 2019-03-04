ebe65fba4caa220de3d58850e737897c6bb7f5e4
Pablo Machado <pablo@tiqets.com>
2019-03-04 11:42:17 +0100

    Fixes incorrect items order after sorting

--- packages/svg-baker/lib/compiler.js
+++ packages/svg-baker/lib/compiler.js
@@ -1,16 +1,14 @@
-const Promise = require('bluebird');
-const merge = require('merge-options');
-const FileRequest = require('./request');
-const RuleSet = require('./rules');
-const Sprite = require('./sprite');
-const SpriteSymbol = require('./symbol');
-const symbolFactory = require('./symbol-factory');
-const spriteFactory = require('./sprite-factory');
+const Promise = require("bluebird");
+const merge = require("merge-options");
+const FileRequest = require("./request");
+const RuleSet = require("./rules");
+const Sprite = require("./sprite");
+const SpriteSymbol = require("./symbol");
+const symbolFactory = require("./symbol-factory");
+const spriteFactory = require("./sprite-factory");
 
 const defaultConfig = {
-  rules: [
-    { test: /\.svg$/, value: 'sprite.svg' }
-  ],
+  rules: [{ test: /\.svg$/, value: "sprite.svg" }],
   symbolFactory,
   spriteFactory
 };
@@ -31,7 +29,7 @@ class Compiler {
   }
 
   constructor(cfg = {}) {
-    const config = this.config = merge(defaultConfig, cfg);
+    const config = (this.config = merge(defaultConfig, cfg));
     this.rules = new RuleSet(config.rules);
     this.symbols = [];
   }
@@ -48,25 +46,34 @@ class Compiler {
     const request = new FileRequest(path);
     const options = { id, request, content, factory };
 
-    const existing = symbols.find(s => s.request.equals(request));
-    const existingIndex = existing ? symbols.indexOf(existing) : -1;
-    const allExceptCurrent = symbols
-      .filter(s => s.request.fileEquals(request) && !s.request.queryEquals(request))
-      .map(symbol => ({ symbol, index: symbols.indexOf(symbol) }));
-
-    return SpriteSymbol.create(options).then((newSymbol) => {
+    return SpriteSymbol.create(options).then(newSymbol => {
+      const existing = symbols.find(s => s.request.equals(request));
       if (!existing) {
         symbols.push(newSymbol);
         Compiler.sortSymbols(symbols);
         return newSymbol;
       }
 
+      const existingIndex = existing ? symbols.indexOf(existing) : -1;
+      const allExceptCurrent = symbols
+        .filter(
+          s => s.request.fileEquals(request) && !s.request.queryEquals(request)
+        )
+        .map(symbol => ({ symbol, index: symbols.indexOf(symbol) }));
+
       symbols[existingIndex] = newSymbol;
       Compiler.sortSymbols(symbols);
 
       return Promise.map(allExceptCurrent, ({ symbol, index }) => {
-        const opts = { id: symbol.id, request: symbol.request, content, factory };
-        return SpriteSymbol.create(opts).then(created => symbols[index] = created);
+        const opts = {
+          id: symbol.id,
+          request: symbol.request,
+          content,
+          factory
+        };
+        return SpriteSymbol.create(opts).then(
+          created => (symbols[index] = created)
+        );
       }).then(() => newSymbol);
     });
   }
@@ -79,20 +86,20 @@ class Compiler {
     const rules = this.rules.rules;
     const factory = this.config.spriteFactory;
 
-    return Promise.map(rules, (rule) => {
+    return Promise.map(rules, rule => {
       const spriteSymbols = [];
       const filename = rule.uri;
 
-      symbols.forEach((symbol) => {
+      symbols.forEach(symbol => {
         const isMatch = rule.match(symbol.request.file);
         if (isMatch) {
           spriteSymbols.push(symbol);
         }
       });
 
-      return spriteSymbols.length > 0 ?
-        Sprite.create({ symbols: spriteSymbols, filename, factory }) :
-        null;
+      return spriteSymbols.length > 0
+        ? Sprite.create({ symbols: spriteSymbols, filename, factory })
+        : null;
     }).filter(result => result !== null);
   }
 }
